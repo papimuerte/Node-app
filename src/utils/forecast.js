42c898189d9604a771aaccea8c4a8bd3d7722033
papimuerte <nate@nateward.net>
2020-07-30 13:58:54 -0600

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=4a454076d200c25af33fe8430a9e3ef5&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' out.')
+        }
+    })
+
+}
+
+module.exports = forecast
